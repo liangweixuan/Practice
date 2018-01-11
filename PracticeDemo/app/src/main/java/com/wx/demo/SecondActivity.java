069cdaa6532a911573c739456cda6ed4468df1a1
corn <weixiaof@desay.com>
2018-01-11 14:44:57 +0800

    添加内容

--- PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
+++ PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
@@ -1,20 +1,24 @@
 package com.wx.demo;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 /**
  * Created by corn on 2018/1/11.
  */
 
-public class SecondActivity extends Activity{
+public class SecondActivity extends AppCompatActivity{
+
+    private TextView tv_text;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e("SecondActivity ","onCreate");
         setContentView(R.layout.activity_second);
+        tv_text = findViewById(R.id.tv_text);
     }
 }
