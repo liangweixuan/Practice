6979a823c709dc28556ace05cef42c33c2087c29
corn <weixiaof@desay.com>
2018-01-11 14:34:09 +0800

    jjjj

--- PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
+++ PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
@@ -3,6 +3,7 @@ package com.wx.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by corn on 2018/1/11.
@@ -13,6 +14,7 @@ public class SecondActivity extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this,"aa",Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_second);
     }
 }
