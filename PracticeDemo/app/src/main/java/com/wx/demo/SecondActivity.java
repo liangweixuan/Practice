7f3ffa53d04ab2aa8c3f62536d68f5738c5249ce
corn <weixiaof@desay.com>
2018-01-11 14:55:04 +0800

    添加内容2

--- PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
+++ PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -21,6 +20,6 @@ public class SecondActivity extends AppCompatActivity{
         Log.e("SecondActivity ","onCreate");
         setContentView(R.layout.activity_second);
         tv_text = findViewById(R.id.tv_text);
-        ImageView imageView = new ImageView(this);
+        TextView text = new TextView(this);
     }
 }
