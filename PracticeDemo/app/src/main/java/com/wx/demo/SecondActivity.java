a8ef557e8db5ebc5aca764f6c63fe3ad39063d66
corn <weixiaof@desay.com>
2018-01-11 14:30:07 +0800

    修改OnCreate

--- PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
+++ PracticeDemo/app/src/main/java/com/wx/demo/SecondActivity.java
@@ -3,6 +3,7 @@ package com.wx.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by corn on 2018/1/11.
@@ -13,6 +14,7 @@ public class SecondActivity extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("SecondActivity ","onCreate");
         setContentView(R.layout.activity_second);
     }
 }
